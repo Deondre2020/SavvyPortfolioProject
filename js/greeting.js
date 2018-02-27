bbc3ddc991c2294c2daa4a5d5a8222429472e346
Marquis Patton <pattonmarquis2020@gmail.com>
2018-02-27 11:15:55 -0600

    Adding Javascript comments to JS file

--- js/greeting.js
+++ js/greeting.js
@@ -1,14 +1,26 @@
--var greetUser = function greetUser(){
+var greetUser = function greetUser(){
   var firstName = prompt("What is your name for real this time");
 
-  if(firstName ===""){
+  if(firstName === ""){
     greetUser();
   } else {
-
-    alert("hello " + firstName);
+    document.querySelector("h1").textContent = " Welcome " + firstName;
    }
-}
+};
 
 greetUser();
 
+//-----------------------Examples!!---------------------------//
+
 //LEAN ON THIS HEAVILY WHEN USING JAVASCRIPT!!!//
+
+//var logPrimitives = function logPrimitives() {
+//console.log(Marines + Deondre);
+//}
+
+//var mySecondArray = ['Car', 'America']//
+
+//document.querySelector//
+
+//document.querySelector("h1").textContent = "Deadmanwalkin" use this to...
+//change text on a website through a console//
