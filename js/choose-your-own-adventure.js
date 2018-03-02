eeb88bd9fbaf51c6b63e70aecbf53c72ab933911
Marquis Patton <pattonmarquis2020@gmail.com>
2018-03-02 11:23:28 -0600

    moved choose your own adventure file and made changes

---
+++ js/choose-your-own-adventure.js
@@ -0,0 +1,34 @@
+var storyBeginning = { "start": "Your car breaks down at a road far from the nearest town and you are surrounded on both sides by thick forest and a severe blizzard is moving toward you.",
+};
+
+var storyMiddle = { "callForHelp": "You realize that you forgot your phone charger",
+};
+
+var storyMiddle1 = { "takeShortcut": "As you scan the surroundings you see that the nearest city skyline is located past a dense forest. You see this as your only option and you advance toward it",
+};
+
+var storyMiddle2 = { "wait": "You wait for a driver to come pass you by but nobody comes and as time passes your car continues to keep getting colder",
+};
+
+var storyEnding = { "fallAsleep": "You fall asleep and never wake up again"
+};
+
+var choice = prompt( story.start );
+
+alert( story.start );
+
+
+console.log( choice );
+console.log( story );
+
+// ------------------------additional story nodes----------------------------- //
+
+// "start": "You're car breaks down at a road that is surrounded on both sides by thick forest and you forgot your phone charger",
+// "look": "As you scan the surroundings you see that the nearest city skyline is located past a dense forest",
+// "walk": "Feeling adventurous you advance farther into the forest",
+// "formulateAPlan": "Sit, rest, and think and realize that your best option is to use the stars to help you stay on a straight path",
+// "callForHelp": "You call for help in vain until you give up and realize nobody is there to help",
+// "runFrankikly": "You run and tire yourself",
+// "listenForAnimals": "You hear a wolf howl and avoid it's general direction",
+// "fallAsleep": "You fall asleep and never wake up"
+// "wait": "You wait for a driver to come pass you by but nobody comes and as time passes your car continues to keep getting colder"
